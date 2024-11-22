9c10b54b6947c93052d036b1b54c43cf92dee71d
OmPrajapati7901 <omprajapati7901@gmail.com>
2024-11-21 19:13:27 -0700

    added AuditMixin

--- app/models.py
+++ app/models.py
@@ -1,25 +1,28 @@
 from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
 # from sqlalchemy.ext.declarative import declarative_base
-import datetime
 from sqlalchemy.orm import relationship
 # from app.database import Base;
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
-# Base = declarative_base()
 from settings import DB_USER
 
 Base = declarative_base()
 
-
+class AuditMixin:
+    created_by = Column(String(100), nullable=False, default=DB_USER)
+    updated_by = Column(String(100), nullable=False, default=DB_USER)
+    created_at = Column(DateTime, nullable=False, default=func.now())
+    updated_at = Column(DateTime, nullable=False, default=func.now(), onupdate=func.now())
 
 # Table for Businesses
-class Business(Base):
+class Business(Base, AuditMixin):
     __tablename__ = "businesses"
 
     business_id = Column(Integer, primary_key=True, autoincrement=False)  # No auto-increment to match schema
     business_name = Column(String(255), nullable=False)
-    created_at = Column(DateTime, default=func.now(), nullable=False)
-    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
+
+    # created_at = Column(DateTime, default=func.now(), nullable=False)
+    # updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
     # Relationship to Business_Symptoms
     business_symptoms = relationship("BusinessSymptom", back_populates="business")
 
@@ -27,13 +30,13 @@ class Business(Base):
 # Table for Symptoms
 
 
-class Symptom(Base):
+class Symptom(Base, AuditMixin):
     __tablename__ = "symptoms"
 
     symptom_code = Column(String(50), primary_key=True)
     symptom_name = Column(String(255), nullable=False)
-    created_at = Column(DateTime, default=func.now(), nullable=False)
-    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
+    # created_at = Column(DateTime, default=func.now(), nullable=False)
+    # updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
 
     # Relationship to Business_Symptoms
     business_symptoms = relationship("BusinessSymptom", back_populates="symptom")
@@ -42,7 +45,7 @@ class Symptom(Base):
 
 
 # Linking table for Businesses and Symptoms
-class BusinessSymptom(Base):
+class BusinessSymptom(Base, AuditMixin):
     __tablename__ = "business_symptoms"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -50,8 +53,8 @@ class BusinessSymptom(Base):
     symptom_code = Column(String(50), ForeignKey("symptoms.symptom_code"), nullable=False)
     symptom_diagnostic = Column(Boolean, nullable=False)
 
-    created_at = Column(DateTime, default=func.now(), nullable=False)
-    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
+    # created_at = Column(DateTime, default=func.now(), nullable=False)
+    # updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
 
     # Relationships
     business = relationship("Business", back_populates="business_symptoms")
